87dea92228a3c08a3f9f5158cd7f49c3c6951aa5
guido <guido.vespignani@gmail.com>
2016-09-21 23:09:36 -0300

    commit
    commit

---
+++ util/main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "util.h"
+
+
+
+int main()
+{
+  char seguir='s';
+
+  while(seguir == 's')
+  {
+      switch(menu(char texto[],int,int))
+      {
+
+	
+	
+
+	 
+       }
+
+   }
+
+  return 0;
+
+} 
+   
