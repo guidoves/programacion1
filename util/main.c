6b5485561d088e40fe1f73bbfb4975744d5b076e
guido <guido.vespignani@gmail.com>
2016-10-05 00:41:38 -0300

    commit

--- util/main.c
+++ util/main.c
@@ -14,13 +14,35 @@ int main()
       switch(menu(char texto[],int,int))
       {
 
-	
-	
+		case 1:
+		break;
+		case 2:
+		break;
+		case 3:
+		break;
+		case 4:
+		break;
+		case 5:
+		break;
+		case 6:
+		break;
+		case 7:
+		break;
+		case 8:
+		break;
 
-	 
+	
        }
 
+      	printf("Precione una tecla para continuar...");
+        flush_in();
+        system("clear");
+
+
    }
+
+   
+
 
   return 0;
 
--- util/util.c
+++ util/util.c
@@ -5,6 +5,198 @@
 #include <math.h>
 #include "util.h"
 
+/**
+ * \brief Limpia el stdin, similar a fflush
+ * \param -
+ * \return -
+ *
+ */
+void cleanStdin(void)
+{
+    int c;
+    do {
+        c = getchar();
+    } while (c != '\n' && c != EOF);
+}
+
+/**
+ * \brief Solicita un numero entero al usuario y lo valida
+ * \param requestMessage Es el mensaje a ser mostrado para solicitar el dato
+ * \param requestMessage Es el mensaje a ser mostrado en caso de error
+ * \return El número ingresado por el usuario
+ *
+ */
+
+int getValidInt(char requestMessage[],char errorMessage[], int lowLimit, int hiLimit)
+{
+    char auxStr[256];
+    int auxInt;
+    while(1)
+    {
+        if (!getStringNumeros(requestMessage,auxStr))
+        {
+            printf ("%s\n",errorMessage);
+            continue;
+
+        }
+        auxInt = atoi(auxStr);
+        if(auxInt < lowLimit || auxInt > hiLimit)
+        {
+            printf ("El numero del debe ser mayor a %d y menor a %d\n",lowLimit,hiLimit);
+            continue;
+
+        }
+        return auxInt;
+
+    }
+
+/**
+ * \brief Verifica si el valor recibido contiene solo números, + y -
+ * \param str Array con la cadena a ser analizada
+ * \return 1 si contiene solo numeros, espacios y un guion.
+ *
+ */
+int esTelefono(char str[])
+{
+   int i=0;
+   int contadorGuiones=0;
+   while(str[i] != '\0')
+   {
+       if((str[i] != ' ') && (str[i] != '-') && (str[i] < '0' || str[i] > '9'))
+           return 0;
+       if(str[i] == '-')
+            contadorGuiones++;
+       i++;
+   }
+   if(contadorGuiones==1) // debe tener un guion
+        return 1;
+
+    return 0;
+}
+
+/**
+ * \brief Verifica si el valor recibido contiene solo letras
+ * \param str Array con la cadena a ser analizada
+ * \return 1 si contiene solo ' ' y letras y 0 si no lo es
+ *
+ */
+int esSoloLetras(char str[])
+{
+   int i=0;
+   while(str[i] != '\0')
+   {
+       if((str[i] != ' ') && (str[i] < 'a' || str[i] > 'z') && (str[i] < 'A' || str[i] > 'Z'))
+           return 0;
+       i++;
+   }
+   return 1;
+}
+
+/**
+ * \brief Verifica si el valor recibido es numérico aceptando flotantes
+ * \param str Array con la cadena a ser analizada
+ * \return 1 si es númerico y 0 si no lo es
+ *
+ */
+
+int esNumericoFlotante(char str[])
+{
+   int i=0;
+   int cantidadPuntos=0;
+   while(str[i] != '\0')
+   {
+       if (str[i] == '.' && cantidadPuntos == 0)
+       {
+           cantidadPuntos++;
+           i++;
+           continue;
+
+       }
+       if(str[i] < '0' || str[i] > '9')
+           return 0;
+       i++;
+   }
+   return 1;
+}
+
+x* findById(x* pArray, int length,int id)
+{
+    x *retorno=NULL;
+    int i;
+
+    if(pArray != NULL && length > 0)
+    {
+
+        for(i=0;i<length;i++)
+        {
+            if(pArray[i].id==id)
+            {
+                retorno=&pArray[i].id;
+
+                break;
+
+            }
+        }
+    }
+
+    return retorno;
+
+
+
+}
+
+/**
+* \brief Verifica que exista un lugar activo o cargado
+* \param recibe la estructura completa
+* \param recibe el largo de la estructura
+* \return devuelve 1 si hay lugares activos o cargados y 0 si no los hay
+*
+*/
+
+int chequearActivos(x array[],int largo)
+{
+    int i;
+    int activos=0;
+
+    for(i=0;i<largo;i++)
+    {
+        if(array[i].estado==1)
+        {
+            activos=1;
+
+            break;
+        }
+    }
+
+    return activos;
+}
+
+
+/**
+* \brief Obtieve el primer espacio libre
+* \param array Recibe el array
+* \param largo Recibe el largo del array
+* \return devuele el indice libre, sino encontro libres devuelve -1
+*
+*/
+
+int obtenerEspacioLibre(x array[],int largo)
+{
+    int i;
+    int indice=-1;
+
+    for(i=0;i<largo;i++)
+    {
+        if(array[i].estado==0)
+        {
+            indice=i;
+
+            break;
+        }
+    }
+    return indice;
+}
+
 void setArray(x array[],int largo,int valor)
 {
 	int i;
@@ -14,66 +206,96 @@ void setArray(x array[],int largo,int valor)
 		array[i]=valor;
 	}
 }
+/**
+* \brief Funcion para validar una cadena de caracteres (No numeros) 
+* \param buffer Ingreso del usuario.
+* \param min valor minimo de la longitud de la cadena.
+* \param max valor maximo de la longitud de la cadena.
+* \param largo largo de la cadena de caracteres.
+* \return Devuelve 0 si el ingreso es valido o devuelve 0 si es invalido.
+*
+*/
 
 
-
-
-
-int validarString(char buffer[],int min,int max)
+int validarString(char buffer[],int min,int max,int largo)
 {
-    int aux=0;
-    int j;
+    int aux=-1;
+    int i,j;
     j=strlen(buffer);
 
     if(j >= min && j <= max)
     {
-        aux=1;
+      aux=0;
+
+      for(i=0;i<largo;i++)
+      {
+        if(isdigit(buffer[i])==1)
+        {
+          aux=-1;
+
+          break;
+        }
+        
+      }
+
+
     }
 
     return aux;
 
 }
 /**
-* \brief Funcion para verificar el tamaño del ingreso del usuario.
-* \param Entrada el usuario
-* \param Tamaño minimo.
-* \param Tamaño maximo.
-* \return Devuelve 1 si el String es correcto o devuelve 0 si es incorrecto.
+* \brief Funcion para validar un caracter por un determinado valor
+* \param buffer Ingreso del usuario.
+* \param char x caracter valido.
+* \param char y caracter valido.
+* \return Devuelve 0 si el ingreso es valido o devuelve -1 si es invalido.
 *
 */
 
-int validarChar(char buffer[])
+int validarChar(char buffer[], char x,char y)
 {
-    int aux=0;
+    int retorno=-1;
     int j;
+    char aux;
+
     j=strlen(buffer);
 
     if(j==1)
     {
-        aux=1;
+      aux=buffer[0];
+
+      if(aux == x || aux == y)
+      {
+        aux=-1;
+
+      }
+
+        
     }
 
-    return aux;
-
+    return retorno;
 
 }
 /**
-* \brief Funcion para verificar si el usuario ingreso un char
-* \param Recibe la entrada por teclado del usuario
-* \return Devuelve 1 si es un char y devuelve 0 si no lo es.
+* \brief Valida si el usuario ingreso un entero positivo
+* \param buffer Recibe el ingreso del usuario.
+* \param min Recibe el valor minimo valido
+* \param max Recibe el valor maximo valido.
+* \return Devuelve -1 si la cadena posee letras o signos, y devuelve 0 si la cadena posee numeros.
 *
 */
 
 int validarInt(char buffer[],int min,int max)
 {
-    int aux=1; // RETURN
+    int aux=0; // RETURN
     int j; // AUXILIAR
     int i=0; // AUXILIARCONTADOR
     int numero;
 
     j=strlen(buffer); // CARGA EN AUX LA CANTIDAD DE ELEMENTOS DEL ARRAY
 
-	while( i<j && aux == 1)
+	while( i<j && aux == 0)
     {
 	  if(isdigit(buffer[i])!=0) // isdigit devuelve 1 si es numero o 0 sino lo es.
       {
@@ -81,16 +303,16 @@ int validarInt(char buffer[],int min,int max)
       }
 	  else
       {
-	      aux=0;
+	      aux=-1;
       }
 	}
-	if(aux==1)
+	if(aux==0)
 	{
         numero=cadena_entero(buffer);
 
         if(numero < min || numero > max)
         {
-            aux=0;
+            aux=-1;
         }
 
 	}
@@ -99,12 +321,11 @@ int validarInt(char buffer[],int min,int max)
 }
 
 /**
-* \brief Valida si el usuario ingreso un entero positivo
-* \param Recibe como parametro una cadena de caracteres
-* \return Devuelve 0 si la cadena posee letras o signos, y devuelve 1 si la cadena posee numeros.
+* \brief Trasnforma una cadena de numeros a un entero(int).
+* \param Recibe la cadena de numeros.
+* \return Devuelve la cadena transformado a un entero.
 *
 */
-
 
 
 int cadena_entero(char numero[])
@@ -125,11 +346,13 @@ int cadena_entero(char numero[])
 }
 
 /**
-* \brief Trasnforma una cadena de numeros a un entero(int).
-* \param Recibe la cadena de numeros.
-* \return Devuelve la cadena transformado a un entero.
-*
+* \brief Funcion para crear un menu de opciones
+* \param Son los items del menu de opciones.
+* \param Define el rango minimo de opciones.
+* \param Define el rango maximo de opciones.
+* \return Devuelve la opcion elegida por el usuario.
 */
+
 
 int menu(char texto[],int rangoOpcionMin,int rangoOpcionMax)
 {
@@ -140,7 +363,7 @@ int menu(char texto[],int rangoOpcionMin,int rangoOpcionMax)
   printf("Opcion: ");
   gets(buffer);
 
-  while(validarInt(buffer,rangoOpcionMin,rangoOpcionMax)!=1)
+  while(validarInt(buffer,rangoOpcionMin,rangoOpcionMax)!=0)
   {
     printf("Ingreso invalido. Ingrese opcion valida.\n");
     printf("Opcion: ");
@@ -154,15 +377,9 @@ int menu(char texto[],int rangoOpcionMin,int rangoOpcionMax)
 
 
 }
-
 /**
-* \brief Funcion para crear un menu de opciones
-* \param Son los items del menu de opciones.
-* \param Define el rango minimo de opciones.
-* \param Define el rango maximo de opciones.
-* \return Devuelve la opcion elegida por el usuario.
+* \brief Funcion para limpiar el buffer en cualquier SO.
 */
-
 
 void flush_in()
 {
@@ -171,7 +388,5 @@ void flush_in()
   while( (ch = fgetc(stdin)) != EOF && ch != '\n' ){}
 }
 
-/**
-* \brief Funcion para limpiar el buffer en cualquier SO.
-*/
+
 
